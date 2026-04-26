933ac11f93e9fe12d1320e61dcfcde18979c8427
sv8015 <99641879+sv8015@users.noreply.github.com>
2026-04-26 22:45:15 +0530

    Create ConfigReader.java

---
+++ src/main/java/utils/ConfigReader.java
@@ -0,0 +1,23 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties prop;
+
+    static {
+        try {
+            prop = new Properties();
+            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            prop.load(fis);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load config file");
+        }
+    }
+
+    public static String get(String key) {
+        return prop.getProperty(key);
+    }
+}
